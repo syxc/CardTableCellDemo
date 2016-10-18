3d3145252bf5bb2b9d8b30e1f60b507ce7e14e0c
syxc <gaibing2009@gmail.com>
2016-10-19 01:08:08 +0800

    Initial Commit

---
+++ CardTableCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardTableCellDemo
+//
+//  Created by syxc on 2016/10/19.
+//  Copyright © 2016年 syxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardTableCellDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardTableCellDemo
+//
+//  Created by syxc on 2016/10/19.
+//  Copyright © 2016年 syxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
